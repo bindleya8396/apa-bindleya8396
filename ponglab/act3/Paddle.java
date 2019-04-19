17739f22aef6feb2dd5c717c2b42153e5c91a669
bindleya8396 <42781299+bindleya8396@users.noreply.github.com>
2019-04-19 14:19:49 -0700

    Update Paddle.java

--- ponglab/act3/Paddle.java
+++ ponglab/act3/Paddle.java
@@ -18,30 +18,56 @@ public class Paddle extends Block
 
 
   //add the other Paddle constructors
-
-
-
-
-
-
-
-
-
+   public Paddle(int x, int y) {
+     super(x, y);
+     speed = 5;
+   }
+   public Paddle(int x, int y, int spd) {
+     super(x, y);
+     speed = spd;
+   }
+   public Paddle(int x, int y, int w, int h) {
+     super(x, y, w, h);
+     speed = 5;
+   }
+   public Paddle(int x, int y, int w, int h, int spd) {
+     super(x, y, w, h);
+     speed = spd;
+   }   
+   public Paddle(int x, int y, int w, int h, Color col) {
+     super(x, y, w, h, col);
+     speed = 5;
+   }
+   public Paddle(int x, int y, int w, int h, Color col, int spd) {
+     super(x, y, w, h, col);
+     speed = spd;
+   }
+   
+   //set method
+   public void setSpeed(int s)
+   {
+     speed = s; 
+   }
 
   public void moveUpAndDraw(Graphics window)
   {
-
-
+      setY(getY() + speed);
+      draw(window);
   }
 
   public void moveDownAndDraw(Graphics window)
   {
-
-
+      setY(getY() - speed);
+      draw(window);
   }
 
   //add get methods
-   
+  public int getSpeed() {
+     return speed;
+   }
    
   //add a toString() method
+  public String toString() {
+     return super.toString() + " " + speed;
+   }
 }
