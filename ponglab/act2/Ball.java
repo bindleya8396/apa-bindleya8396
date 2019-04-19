7b227fc98dbcf78863b71cbaac9727912aec1bf0
bindleya8396 <42781299+bindleya8396@users.noreply.github.com>
2019-04-19 14:16:55 -0700

    Update Ball.java

--- ponglab/act2/Ball.java
+++ ponglab/act2/Ball.java
@@ -16,45 +16,79 @@ public class Ball extends Block
     xSpeed = 3;
     ySpeed = 1;
   }
-
   //add the other Ball constructors
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-           
+  public Ball(int x, int y) {
+     super(x, y);
+     xSpeed = 3;
+     ySpeed = 1;
+   }
+   public Ball(int x, int y, int w) {
+     super(x, y, w);
+     xSpeed = 3;
+     ySpeed = 1;
+   }
+   public Ball(int x, int y, int w, int h) {
+     super(x, y, w, h);
+     xSpeed = 3;
+     ySpeed = 1;
+   }
+   public Ball(int x, int y, int w, int h, Color col) {
+     super(x, y, w, h, col);
+     xSpeed = 3;
+     ySpeed = 1;
+   }
+   public Ball(int x, int y, int w, int h, Color col, int xSpd) {
+     super(x, y, w, h, col);
+     xSpeed = xSpd;
+     ySpeed = 1;
+   }
+   public Ball(int x, int y, int w, int h, Color col, int xSpd, int ySpd) {
+     super(x, y, w, h, col);
+     xSpeed = xSpd;
+     ySpeed = ySpd;
+   }        
+     
   //add the set methods
-   
+  public void setXspeed(int x) {
+     xSpeed = x;
+   }
+   public void setYspeed(int y) {
+     ySpeed = y;
+   }
+   public int getXspeed() {
+     return xSpeed;
+   }
+   public int getYspeed() {
+     return ySpeed;
+   }
 
   public void moveAndDraw(Graphics window)
   {
     //draw a white ball at old ball location
-
-
-    setX(getX()+xSpeed);
+    draw(window);
+    setX(getX() + xSpeed);
     //setY
-
+    setY(getY() + ySpeed);
     //draw the ball at its new location
+    draw(window);
   }
    
   public boolean equals(Object obj)
   {
-
-
-
-
-    return false;
+     Ball boll = (Ball)obj;
+     return xSpeed == boll.getXspeed() && ySpeed == boll.getYspeed();
   }   
 
   //add the get methods
-
+   public int getX() {
+     return xSpeed;
+   }
+   public int getY() {
+     return ySpeed;
+   }
+   
   //add a toString() method
+    public String toString() {
+   	return super.toString() + " " + xSpeed + " " + ySpeed;
+    }
 }
